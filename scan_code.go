bb83219b577c5c437edfa6d5135e7ec2d1e20503
しずえ <cauchymoon@mail.ru>
2021-03-17 15:05:22 +0300

    Angle bracket happy path support.

--- scan_code.go
+++ scan_code.go
@@ -7,6 +7,12 @@ import (
 func ScanCode(code string) []string {
     parts := strings.Split(code, "\"")
 
+    if code != "" && len(parts) == 1 {
+        head := strings.Split(code, "<")
+        parts = head[:1]
+        parts = append(parts, strings.Split(head[1], ">")...)
+    }
+
     if len(parts) == 3 {
         return []string{parts[1]}
     }
