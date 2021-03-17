2e0c8430715d8b66895439bb2f5bbfa5e8c84c7a
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-03-17 14:44:33 +0300

    Parse one line of double-quoted include.

--- scan_code.go
+++ scan_code.go
@@ -1,5 +1,15 @@
 package wsii
 
+import (
+    "strings"
+)
+
 func ScanCode(code string) []string {
+    parts := strings.Split(code, "\"")
+
+    if len(parts) == 3 {
+        return []string{parts[1]}
+    }
+
     return nil
 }
