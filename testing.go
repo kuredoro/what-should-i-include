7c913acf93f7e91ee485be6ec11412423be34c03
しずえ <66309775+kuredoro@users.noreply.github.com>
2021-03-17 15:01:10 +0300

    Refactor: wrote AssertIncludes.

--- testing.go
+++ testing.go
@@ -1,2 +1,42 @@
 package wsii
 
+import (
+    "testing"
+)
+
+func AssertIncludes(t *testing.T, got, want []string) {
+    t.Helper()
+
+    union := make(map[string]int)
+
+    for _, inc := range got {
+        union[inc]++
+    }
+
+    for _, inc := range want {
+        union[inc]--
+    }
+
+    var didntFind, spurious []string
+    for inc, membership := range union {
+        if membership == -1 {
+            didntFind = append(didntFind, inc)
+        }
+
+        if membership == 1 {
+            spurious = append(spurious, inc)
+        }
+    }
+
+    if didntFind != nil {
+        for _, inc := range didntFind {
+            t.Errorf("didn't find header: %q", inc)
+        }
+    }
+
+    if spurious != nil {
+        for _, inc := range spurious {
+            t.Errorf("spuriously found: %q", inc)
+        }
+    }
+}
